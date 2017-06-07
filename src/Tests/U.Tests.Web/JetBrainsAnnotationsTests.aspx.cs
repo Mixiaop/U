fbe755940d4477a6d4b25c465d020849216755a1
Mixiaop <112949824@qq.com>
2017-06-07 11:55:09 +0800

    JetBrains.Annotations Tests

---
+++ src/Tests/U.Tests.Web/JetBrainsAnnotationsTests.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using JetBrains.Annotations;
+
+namespace U.Tests.Web
+{
+    public partial class JetBrainsAnnotationsTests : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloClass hello = new HelloClass() ;
+            hello.Name = null;
+        }
+    }
+
+    public class HelloClass {
+        [NotNull]
+        public string Name { get; set; }
+    }
+}
