d2c220aa551f5444357b0bf768ba114a8a0200db
Mixiaop <112949824@qq.com>
2018-05-03 16:23:50 +0800

    1

---
+++ src/U.Web.IISManage/_Tests.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using U.Utilities.IIS;
+using U.Utilities.Web;
+using U.Utilities;
+
+namespace U.Web.IISManage
+{
+    public partial class _Tests : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var process = Process.GetProcessesByName("w3wp");
+            foreach (Process p in process)
+            {
+                var username = U.Utilities.CommonHelper.GetProcessUsername(p.Id);
+                Response.Write(username + "<br />");
+            }
+
+        }
+    }
+}
